805f874c5b049839fa0e8917f47d8d312d912f63
JonChoi <dudtjd4149@gmail.com>
2021-02-22 17:50:04 +0900

    test

--- index.js
+++ index.js
@@ -27,9 +27,6 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/register', (req, res) => {
 
-  //회원 가입 할때 필요한 정보들을 client에서 가져오면
-  //그것들을 DB에 넣어준다.
-
   const user = new User(req.body)
 
   user.save((err, userInfo) => {
