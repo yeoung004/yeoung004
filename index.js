aa6e512e618029d5ee60dfe8f369ccf2e2b497fa
Jon Choi <dudtjd4149@gmail.com>
2021-02-25 00:35:06 +0900

    have to fix can't login

--- index.js
+++ index.js
@@ -73,16 +73,30 @@ app.post('/login', (req, res) => {
 })
 
 
-app.post('/api/user/auth', auth, (req, res) => {
-
-  
-
+app.get('/api/user/auth', auth, (res, req) => {
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
 })
 
 
-
-
-
+app.get('/api/users/logout', auth, (res, req) => {
+  User.findOneAndUpdate({_id: req.user._id },
+    {token: ""}
+    , (err, user) => {
+      if (err) return res.json({ successs: false, err});
+      return res.status(200).send({
+        success: true
+      })
+    })
+})
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- models/User.js
+++ models/User.js
@@ -76,6 +76,17 @@ userSchema.methods.generateToken = function(cb) {
     })
 }
 
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+    jwt.verity(token, 'secretToken', function(err, decoded) {
+        user.findOne({ "_id": decoded, "token": token }, function (err, user){
+            if (err) return cb(err);
+            cb(null, user)
+        })
+    })
+}
+
+
 const User = mongoose.model('User', userSchema)
 
 module.exports = { User }
